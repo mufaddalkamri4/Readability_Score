dbb802e331cd0f89ae0837b625684eb2fce8eeed
Mufaddal Kamri <mufaddalkamri4@gmail.com>
2021-02-17 23:10:42 +0530

    implemented stage 2

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,19 +1,35 @@
 package readability;
 
+import java.io.File;
 import java.util.Scanner;
-
-enum ReadabilityScore {
-    HARD,
-    EASY
-}
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String inputString = scanner.nextLine();
+        File file = new File(args[0]);
+        String inputString = "";
+        Scanner scanner = null;
 
-        if (inputString.length() > 100) {
+        try {
+            scanner = new Scanner("./" + file);
+        } catch (Exception e) {
+            inputString = scanner.nextLine();
+        }
+
+        String[] sentences = inputString.split("[\\.!\\?]\\s");
+
+        int totalWordCount = 0;
+        
+        for (String sentence : sentences) {
+            String[] words = sentence.split("\\w+[.?!]?");
+//            System.out.printf("%s : %d\n", sentence, words.length);
+            totalWordCount += words.length;
+        }
+        
+        double averageWordCount = (double) totalWordCount / sentences.length;
+//        System.out.println(averageWordCount);
+
+        if (averageWordCount > 10.0) {
             System.out.println("HARD");
         } else {
             System.out.println("EASY");
